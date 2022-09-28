eeb37005053cbb2e891d55ae8c5facb9a3288e4e
Fran Lucena <fran.lucena@gmail.com>
2022-09-28 19:58:49 +0200

    ADD Ejemplo 2

--- Tema1/Ejemplo02/app/src/main/java/hl/t1/ejemplo02/MainActivity.java
+++ Tema1/Ejemplo02/app/src/main/java/hl/t1/ejemplo02/MainActivity.java
@@ -1,26 +1,14 @@
-package hl.t1.ejemplo1;
+package hl.t1.ejemplo02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
-    int [] banderas= new int[]{R.drawable.spain,
-            R.drawable.usa};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public void loadImg(){
-        ImageButton btn1, btn2;
-        btn1=findViewById(banderas[1]);
-        btn1=findViewById(banderas[2]);
-    }
-
 }
