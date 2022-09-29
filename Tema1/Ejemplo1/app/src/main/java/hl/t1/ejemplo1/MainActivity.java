9f7e877809c192b707007a718472384d5b53677b
Fran Lucena <fran.lucena@gmail.com>
2022-09-29 12:51:36 +0200

    UP atributos de los widget

--- Tema1/Ejemplo1/app/src/main/java/hl/t1/ejemplo1/MainActivity.java
+++ Tema1/Ejemplo1/app/src/main/java/hl/t1/ejemplo1/MainActivity.java
@@ -7,20 +7,22 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Pista 1
     int [] banderas= new int[]{R.drawable.spain,
             R.drawable.usa};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Pista 2
+        ImageButton boton1, boton2;
+        boton1=findViewById(banderas[1]);
+        boton2 =findViewById(banderas[2]);
+
     }
 
-    public void loadImg(){
-        ImageButton btn1, btn2;
-        btn1=findViewById(banderas[1]);
-        btn1=findViewById(banderas[2]);
-    }
+
 
 }
